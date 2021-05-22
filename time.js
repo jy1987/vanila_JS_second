a860d81a2f2948149a9d8c7319f293d1485d4f6a
jy1987 <87jy1015@gmail.com>
2021-05-22 17:01:13 +0900

    add timer

--- time.js
+++ time.js
@@ -6,7 +6,23 @@ const dDay = new Date("2021-12-25T00:00:00")
 function time(){
     const now = Date.now();
     const diff = dDay-now;
+    //console.log(diff);
+    const per = Math.floor(diff/1000);
+    const day = Math.floor(per/(60*60*24));
+    //console.log(day);
+    const hour = Math.floor((per%(60*60*24))/(60*60));
+    //console.log(hour);
+    const minute = Math.floor((per%(60*60))/60);
+    //console.log(minute);
+    const second = (per%60);
+    //console.log(second);
+    getTime.innerText=`${day}days ${ hour < 10 ? `0${hour}` : `${hour}`}h ${minute < 10 ? `0${minute}` : `${minute}`}m ${second <10 ? `0${second}` : `${second}`}s`;
     
+    if (second % 2 ===1) {
+        getTime.style.color ='pink'
+    } else {
+        getTime.style.color ='orange'
+    }
 }
 
 function init(){
